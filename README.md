cddd0c91192344dfb5d3fe1dbce1a8811d35850b
Rahul Johnny <royjohnny63@gmail.com>
2017-08-04 21:44:40 +0600

    Nothing special

---
+++ README.md
@@ -0,0 +1,3 @@
+# EightPuzzle
+It's a simple 8-puzzle game based on string matching.
+A better one can be made adding A* search/other game theory algo.
---
+++ src/myimageapp/myimageapp.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package myimageapp;
+
+
+/**
+ *
+ * @author JoHnNyStEiN
+ */
+public class myimageapp{ //why this class
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+   main ob=new main();
+   ob.setVisible(true);
+    }
+    
+}
+
